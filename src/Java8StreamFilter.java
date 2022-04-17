45b05b25ac2daa316109c05bfb6d94644e41f95e
ricken07 <ricken.bazolo@gmail.com>
2022-04-18 00:08:19 +0200

    la methode filter en action

--- src/Java8StreamFilter.java
+++ src/Java8StreamFilter.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Java8StreamFilter {
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+/**
+ * @author autourducode
+ */
+public class Java8StreamFilter {
+    public static void main(String[] args) {
+        // Liste des fruits
+        Stream<String> fruits = Stream.of("Pomme", "Mangue", "Fraise", "Banane", "Avocat", "Framboise");
+        // filtre des fruits contenant 'm' dans le nom
+        fruits.filter(f -> f.contains("m")).forEach(System.out::println);
+        // Affiche: Pomme, Framboise
+        // filtre des fruits ayant plus de 6 caractères dans le nom
+        fruits.filter(f -> f.length() > 6).forEach(System.out::println);
+        // Affiche: Framboise
+    }
 }
